e15fb2500646a7740da1351484ada29dee77685f
Saurabh Pathak <pathaks@tcd.ie>
2016-10-28 17:03:30 +0100

    fixing directory structure

---
+++ rtr-2/Source.cpp
@@ -0,0 +1,29 @@
+//using namespace mapping;
+//using namespace cubemap;
+
+// Std. Includes
+#include <string>
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+// GL includes
+#include <lgl/shader.h>
+#include <lgl/camera.h>
+
+// GLM Mathemtics
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Other Libs
+#include <SOIL.h>
+
+int main3() {
+	//mapping::main();
+	return 0;
+}
